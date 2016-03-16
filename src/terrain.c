bee2a46ecdb3dfe6fd6616b5b7966c43ad3cadaa
Riviere Tristan <NorthRiver@laposte.net>
2016-03-16 14:43:38 +0100

    16/03 15h
    Documentation du code Terrain fini

--- src/terrain.c
+++ src/terrain.c
@@ -6,36 +6,43 @@
 #include <stdlib.h>
 #include "terrain.h"
 
+
 void setXY (int x, int y, terrain * leTerrain){
 
 	leTerrain->dimx = x;
 	leTerrain->dimy = y;
 }
 
+
 void setnomTerrain(char * nom, terrain * leTerrain){
 	int i;
 	for (i=0; i<MAX_CHAR_NOM_TERRAIN; i++){
 		leTerrain->nomTerrain[i] = nom[i];}
 }
+
 void setgrilleXY (int x, int y, terrain * leTerrain, char valeur){
 	leTerrain->grille[x][y] = valeur;
 }
+
 int getX(terrain * leTerrain){
 	return leTerrain->dimx;
 }
+
 int getY(terrain * leTerrain){
 	return leTerrain->dimy;
 }
+
 char * getnom(terrain * leTerrain){
 	return leTerrain->nomTerrain;
 }
+
 char getgrilleXY (int x, int y,terrain * leTerrain){
 	return leTerrain->grille[x][y];
 }
 
 
 
-void TerrainInit (terrain * leTerrain){
+void TerrainInitGrille (terrain * leTerrain){
 	int i,j;
 	char X = 'X';
 	for(i=0;i<(leTerrain->dimx);i++){
@@ -51,7 +58,7 @@ terrain * TerrainCreer (int dimx, int dimy, char nomTerrain[MAX_CHAR_NOM_TERRAIN
 	temp = malloc(sizeof(terrain)); //semble avoir un probleme ici
 	setXY(dimx,dimy,temp);
 	setnomTerrain(nomTerrain,temp);
-	TerrainInit(temp);
+	TerrainInitGrille(temp);
 	return temp;
 }
 
@@ -59,7 +66,7 @@ terrain * TerrainCreer (int dimx, int dimy, char nomTerrain[MAX_CHAR_NOM_TERRAIN
 void TerrainCreerFichier (terrain * leTerrain){
 	FILE * fichier;
 	errno_t err;
-	err = fopen_s(&fichier, "./data/test.terrain","w");
+	//err = fopen_s(&fichier, "./data/test.terrain","w");
 	int i, j;
 	for(i = 0; i<(leTerrain->dimx); i++){
 		for(j = 0; j<leTerrain->dimy; j++){
@@ -69,4 +76,6 @@ void TerrainCreerFichier (terrain * leTerrain){
 	}
 }
 /*
-terrain * TerrainLireFichier (char nomTerrain[MAX_CHAR_NOM_TERRAIN]); */
+terrain * TerrainLireFichier (char nomTerrain[MAX_CHAR_NOM_TERRAIN]);
+A CODER QUAND LE FONCTION DU DESSUS MARCHERA
+*/
--- src/terrain.h
+++ src/terrain.h
@@ -15,22 +15,78 @@ typedef struct MTerrain{
     
 } terrain;
 
+/**
+	@brief Edition de X et de Y dans la structure
+	@param x Entier pour la largeur du terrain
+	@param y Entier pour la hauteur du terrain
+	@param leTerrain Pointeur sur la structure terrain a editer
+*/
 void setXY (int x, int y, terrain * leTerrain);
+/**
+	@brief Edition du nom du terrain dans sa structure
+	@param nom Chaine de carractere ne pouvant dépasser les 101 carractere et definissant le nom du terrain
+	@param leTerrain Pointeur sur la structure terrain a editer
+*/
 void setnomTerrain(char nom[MAX_CHAR_NOM_TERRAIN], terrain * leTerrain);
+/**
+	@brief Edition de la grille du terrain au coordonnées X/Y
+	@param x Valeur ou le point de la grille doit etre edité en X
+	@param y Valeur ou le point de la grille doit etre edité en Y
+	@param leTerrain Pointeur sur la structure terrain a editer
+	@param valeur Carractere definissant ce que serra la case
+*/
 void setgrilleXY (int x, int y, terrain * leTerrain, char valeur);
+/**
+	@brief Recupère la dimension en X de la structure terrain
+	@param leTerrain Pointeur sur la structure terrain a editer
+	@return Entier de la dimension en X du terrain
+*/
 int getX(terrain * leTerrain);
+/**
+	@brief Recupère la dimension en Y de la structure terrain
+	@param leTerrain Pointeur sur la structure terrain a editer
+	@return Entier de la dimension en Y du terrain
+*/
 int getY(terrain * leTerrain);
+/**
+	@brief Recupère le nom de la structure terrain
+	@param leTerrain Pointeur sur la structure terrain a editer
+	@return Chaine de carractere du nom du terrain pointé
+*/
 char * getnom(terrain * leTerrain);
+/**
+	@brief Recupere la valleur de la grille au point X/Y
+	@param x Position en x a recuperer
+	@param y poisition en u a récuperer 
+	@param leTerrain Pointeur sur la structure terrain a editer
+	@return Le carractere de la grille en X/Y
+*/
 char getgrilleXY (int x, int y,terrain * leTerrain);
 
 
-
-void TerrainInit (terrain * leTerrain);
-
+/**
+	@brief Sert a remplir la grille de terrain avec un valeur par défaut
+	@param leTerrain Pointeur sur la structure terrain a editer
+*/
+void TerrainInitGrille (terrain * leTerrain);
+/**
+	@brief Creer un terrain avec les dimension et le nom choisi et initialise sa grille en appelant TerrainInitGrille
+	@param dimx La largeur du terrain
+	@param dimy La haueteur du terrain
+	@param nomTerrain Nom du terrain a creer
+	@return Pointeur vers le terrain nouvellement creer
+*/
 terrain * TerrainCreer (int dimx, int dimy, char nomTerrain[MAX_CHAR_NOM_TERRAIN]);
-
+/**
+	@brief Sauvegarde le terrain pointé dans un fichier (.terrain)
+	@param leTerrain Pointeur sur la structure terrain a sauvegarder
+*/
 void TerrainCreerFichier (terrain * leTerrain);
-
+/**
+	@brief Li le fichier terrain de nom spécifié et le retourne par un pointeur
+	@param nomTerrain Nom du fichier terrain à lire
+	@return Pointeur vers la structure lu dans le fichier
+*/
 terrain * TerrainLireFichier (char nomTerrain[MAX_CHAR_NOM_TERRAIN]);
 
 
