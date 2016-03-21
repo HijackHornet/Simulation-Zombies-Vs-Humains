7356e0b5ad9f5479d323db5716730e1a8b52c381
twoflower42 <leo.gigandon@free.fr>
2016-03-21 12:02:40 +0100

    Ajout de la fonction de déplacement aléatoire et des fonctions propres aux zombies Il faut corriger ces dernières, elles ne vérifient pas les bords de la grille et ne font pas appel aux accesseurs pour les modifications de coordonnées. Ajouter une fonction est dans la grille dans le module terrain ?

--- src/personnage.c
+++ src/personnage.c
@@ -94,3 +94,117 @@ void deplacementDroite_perso(Perso * pPerso, Terrain * pTerrain){
 
     pTerrain -> grille[xPerso][yPerso].persoCase = pPerso;
 }
+
+void deplacementAleatoire_perso(Perso * pPerso, Terrain * pTerrain){
+    srand(time(NULL));
+
+    char direction = rand()%4;
+
+    assert(direction < 4 && direction >= 0);
+
+    if(direction == 0){
+	deplacementBas_perso(pPerso, pTerrain);
+    }
+
+    else if(direction == 1){
+	deplacementHaut_perso(pPerso, pTerrain);
+    }
+
+    else if(direction == 2){
+	deplacementDroite_perso(pPerso, pTerrain);
+    }
+
+    else{
+	deplacementGauche_perso(pPerso, pTerrain);
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//PARTIE ZOMBIE//////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*ERREURS A CORRIGER : NE VERIFIE PAS LES BORDURES DE LA GRILLE
+UTILISER VERIF DU DESSUS
+*/
+
+Perso * contaminationHumain(Perso * pHumain){ //retourne l'humain en mode zombie
+    pHumain -> type = ZOMBIE;
+
+    return pHumain;
+}
+
+char humainEnHaut(Terrain * pTerrain, Coordonnees * coordZombie){
+    if(pTerrain -> grille[(coordZombie -> xCoord)][(coordZombie -> yCoord) + 1].persoCase -> type == CITOYEN \
+       || \
+       pTerrain -> grille[coordZombie -> xCoord][(coordZombie -> yCoord) + 1].persoCase -> type == POLICIER){
+	return 1;
+    }
+    else{
+	return 0;
+    }
+}
+
+char humainEnBas(Terrain * pTerrain, Coordonnees * coordZombie){
+    if(pTerrain -> grille[coordZombie -> xCoord][(coordZombie -> yCoord) - 1].persoCase -> type == CITOYEN \
+       || \
+       pTerrain -> grille[coordZombie -> xCoord][(coordZombie -> yCoord) - 1].persoCase -> type == POLICIER){
+	return 1;
+    }
+    else{
+	return 0;
+    }
+}
+
+char humainAGauche(Terrain * pTerrain, Coordonnees * coordZombie){
+    if(pTerrain -> grille[(coordZombie -> xCoord) - 1][coordZombie -> yCoord].persoCase -> type == CITOYEN \
+       || pTerrain -> grille[(coordZombie -> xCoord) - 1][coordZombie -> yCoord].persoCase -> type == POLICIER){
+	return 1;
+    }
+    else{
+	return 0;
+    }
+}
+
+char humainADroite(Terrain * pTerrain, Coordonnees * coordZombie){
+    if(pTerrain -> grille[(coordZombie -> xCoord) + 1][coordZombie -> yCoord].persoCase -> type == CITOYEN \
+       || pTerrain -> grille[(coordZombie -> xCoord) + 1][coordZombie -> yCoord].persoCase -> type == POLICIER){
+	return 1;
+    }
+    else{
+	return 0;
+    }
+}
+
+Perso * zombieComtamineHumain(Perso * pZombie, Terrain * pTerrain){
+    int xZombie, yZombie;
+    
+    if(humainEnHaut(pTerrain, pZombie -> coord)){
+	xZombie = pZombie -> coord -> xCoord;
+	yZombie = pZombie -> coord -> yCoord;
+	return contaminationHumain(pTerrain -> grille [xZombie][yZombie + 1].persoCase);
+    }
+
+    else if(humainEnBas(pTerrain, pZombie -> coord)){
+	xZombie = pZombie -> coord -> xCoord;
+	yZombie = pZombie -> coord -> yCoord;
+	return contaminationHumain(pTerrain -> grille [xZombie][yZombie - 1].persoCase);
+    }
+
+    else if(humainAGauche(pTerrain, pZombie -> coord)){
+	xZombie = pZombie -> coord -> xCoord;
+	yZombie = pZombie -> coord -> yCoord;
+	return contaminationHumain(pTerrain -> grille [xZombie - 1][yZombie].persoCase);
+    }
+
+    else if(humainAGauche(pTerrain, pZombie -> coord)){
+	xZombie = pZombie -> coord -> xCoord;
+	yZombie = pZombie -> coord -> yCoord;
+	return contaminationHumain(pTerrain -> grille [xZombie + 1][yZombie].persoCase);
+    }
+
+    else{
+	return NULL;
+    }
+}
+
--- src/personnage.h
+++ src/personnage.h
@@ -4,8 +4,9 @@
 #include "coordonnees.h"
 #include "terrain.h"
 #include "assert.h"
+#include "time.h"
 
-enum typePerso {LIBRE, POLICIER, ZOMBIE, CITOYEN};
+enum typePerso {POLICIER, ZOMBIE, CITOYEN};
 
 typedef struct MPerso {
     Coordonnees * coord;
@@ -24,6 +25,18 @@ void deplacementHaut_perso(Perso * pPerso, Terrain * pTerrain);
 
 void deplacementBas_perso(Perso * pPerso, Terrain * pTerrain);
 
+void deplacementAleatoire_perso(Perso * pPerso, Terrain * pTerrain);
 
+Perso * contaminationHumain(Perso * pHumain);
+
+char humainEnHaut(Terrain * pTerrain, Coordonnees * coordZombie);
+
+char humainEnBas(Terrain * pTerrain, Coordonnees * coordZombie);
+
+char humainAGauche(Terrain * pTerrain, Coordonnees * coordZombie);
+
+char humainADroite(Terrain * pTerrain, Coordonnees * coordZombie);
+
+Perso * zombieComtamineHumain(Perso * pZombie, Terrain * pTerrain);
 
 #endif
