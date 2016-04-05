bf3d6740ae741117e8aab19509752ebd2a03058c
Riviere Tristan <NorthRiver@laposte.net>
2016-04-05 09:33:22 +0200

    Sdl Marche CB

--- src/main.c
+++ src/main.c
@@ -4,9 +4,10 @@
 #include "coordonnees.h"
 #include "terrain.h"
 #include "simulation.h"
+#include <SDL2/SDL.h>
 //#include "simulation_ncurses.h"
 
-int main(){
+int main(int argc, char* argv[]){
     //testFonctions_Coord();
     //testFonctions_terr();
     //testFonctions_perso();
