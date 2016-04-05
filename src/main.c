b4fa4af58ab278a40edc5b578a4d0bd2a25eefa5
Riviere Tristan <NorthRiver@laposte.net>
2016-04-05 10:09:08 +0200

    SDL

--- src/main.c
+++ src/main.c
@@ -5,6 +5,13 @@
 #include "terrain.h"
 #include "simulation.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+
+// Taille de la fenêtre : 800x480 pixels
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 480
 //#include "simulation_ncurses.h"
 
 int main(int argc, char* argv[]){
