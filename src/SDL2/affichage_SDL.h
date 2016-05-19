b40b1a99f737fd593109a454d9d90b5f0fd6c1a3
Riviere Tristan <northriver@laposte.net>
2016-05-19 13:32:47 +0200

    DoxyGen affichage_SDL.h

--- src/SDL2/affichage_SDL.h
+++ src/SDL2/affichage_SDL.h
@@ -4,8 +4,28 @@
 #include "../simulation.h"
 #include "initialisation_SDL.h"
 
-void afficherParamFenetre(Simulation * pSim);
-void affichageFenetre(Simulation * pSim);
-void affichageFenetreEditeur();
 void delay(unsigned int frameLimit);
+SDL_Texture *loadImage(char *name);
+/**
+	@brief Dessine la SDL_Texture au coordonée XY
+	@param image Pointeur vers la SDL_texture a dessiner
+	@param x Coordonnées en X ou dessiner
+	@param y Coordonnées en Y ou dessiner
+*/
+void drawImage(SDL_Texture *image, int x, int y); 
+/**
+	@brief Dessine les personnage de la simulation sur la fenetre
+	@param pSim Pointeur vers la simulation à afficher
+*/
+void afficherParamFenetre(Simulation * pSim);
+/**
+	@brief Dessine le terrain de la simulation sur la fenetre
+	@param pSim Pointeur vers la simulation à afficher
+*/
+void affichageFenetre(Simulation * pSim);
+/**
+	@brief Dessine le terrain  vierge pour l'editeur sur la fenetre
+*/
+void affichageFenetreEditeur();
+
 
